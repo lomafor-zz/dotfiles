aae6888ce1cfa1a5b7a25213d47f3d67849c62ab
Jun Chen <lomafor@gmail.com>
2017-12-11 19:08:20 -0800

    added tmux plugins and some configs

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -14,6 +14,8 @@ source $ZSH/oh-my-zsh.sh
 # }}}
 # General {{{
 
+export WORKSPACE_PATH=$HOME/workspace
+export GOPATH=$WORKSPACE_PATH/go
 export CDPATH=$CDPATH:$HOME:$WORKSPACE_PATH
 
 # History
@@ -22,23 +24,25 @@ setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
 setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
 setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
 
+# Disable Ctrl-S for Vim's save-file binding
+stty -ixon
+
 # }}}
 # Alias {{{
 
-alias v-zshrc="vim ~/.zshrc"
-alias v-vimrc="vim ~/.vimrc"
-alias s-zshrc="source ~/.zshrc"
 alias lss='ls -AlhtG'
 alias v='vim -p'
 
-## Print directories tree
-alias find-dir="find ./ -type d | sed -e 's/[^-][^\/]*\//--/g;s/--/ |-/'"
+alias a-edit-zshrc="vim ~/.zshrc"
+alias a-edit-vimrc="vim ~/.vimrc"
+alias a-source-zshrc="source ~/.zshrc"
 
-## Kill all java process 
-alias kill-java='ps -ef | grep java | grep -v grep | grep -v CrashPlanService | awk "{print \$2}" | xargs kill -9 && echo "All Java are dead, for now..."'
+alias a-ssh-base='ssh root@dragonbase'
+alias a-ssh-base-main='ssh root@dragonbase_main'
 
-## Print java process
-alias ps-java='ps -ef | grep java | grep -v grep | grep -v CrashPlan'
+alias a-find-dir="find ./ -type d | sed -e 's/[^-][^\/]*\//--/g;s/--/ |-/'"
+alias a-kill-java='ps -ef | grep java | grep -v grep | grep -v CrashPlanService | awk "{print \$2}" | xargs kill -9 && echo "All Java are dead, for now..."'
+alias a-ps-java='ps -ef | grep java | grep -v grep | grep -v CrashPlan'
 
 # }}}
 # Functions {{{
@@ -54,6 +58,29 @@ c() {
 
 source ~/.dotfiles/zsh/hotkeys.sh
 source ~/.dotfiles/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# }}}
+# Enhancements {{{
+
+# Change cursor when ZSH VI mode changes between normal and insert
+function zle-keymap-select zle-line-init
+{
+    # change cursor shape in iTerm2
+    case $KEYMAP in
+        vicmd)      print -n -- "\E]50;CursorShape=0\C-G";;  # block cursor
+        viins|main) print -n -- "\E]50;CursorShape=1\C-G";;  # line cursor
+    esac
+
+    zle reset-prompt
+    zle -R
+}
+function zle-line-finish
+{
+    print -n -- "\E]50;CursorShape=0\C-G"  # block cursor
+}
+zle -N zle-line-init
+zle -N zle-line-finish
+zle -N zle-keymap-select
 
 # }}}
 
